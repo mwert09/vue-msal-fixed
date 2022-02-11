19303c8940c5fc85292e9e78c767ffff63c13b74
Mert Kılıç <mert.kilic@albil.com.tr>
2022-02-11 11:30:56 +0300

    fix: typescript unknown type

--- lib/src/main.ts
+++ lib/src/main.ts
@@ -125,7 +125,7 @@ export class MSAL implements MSALBasic {
             const response = await this.lib.acquireTokenSilent(request);
             this.handleTokenResponse(null, response);
             return response;
-        } catch (error) {
+        } catch (error: any) {
             // Upon acquireTokenSilent failure (due to consent or interaction or login required ONLY)
             // Call acquireTokenRedirect
             console.log(error);
@@ -393,7 +393,7 @@ export class MSAL implements MSALBasic {
                         return cb.id === currentCb.id;
                     });
                     this.storeCallbackQueue();
-                } catch (e) {
+                } catch (e: any) {
                     console.warn(`Callback '${cb.id}' failed with error: `, e.message);
                 }
             }
